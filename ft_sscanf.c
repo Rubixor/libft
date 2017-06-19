f879f861931b48ad1f6e3e31400043ad587078a5
Marie DENOYELLE <mdenoyel@e3r9p2.42.fr>
2017-06-19 22:39:39 +0200

    blep

--- ft_sscanf.c
+++ ft_sscanf.c
@@ -3,24 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sscanf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mdenoyel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/16 16:47:52 by snicolet          #+#    #+#             */
-/*   Updated: 2017/06/16 17:46:46 by snicolet         ###   ########.fr       */
+/*   Created: 2017/06/07 14:48:21 by mdenoyel          #+#    #+#             */
+/*   Updated: 2017/06/12 16:04:29 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_sscanf.h"
 #include <stdarg.h>
 
-int		ft_sscanf(const char *s, const char *format, ...);
+int		ft_sscanf_engine(t_scanf *sf, const char *format)
 {
-	va_list		args;
+	(void)sf;
+	return (0);
+}
+
+int		ft_sscanf(const char *s, const char *format, ...)
+{
 	t_scanf		sf;
-	
-	va_start(args, format);
+	va_list		args;
+
 	sf.args = &args;
 	sf.str = s;
 	sf.str_origin = s;
+	va_start(args, format);
+	ft_sscanf_engine(&sf, format);
 	va_end(args);
+	return (0);
 }
