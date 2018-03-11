903e2305412c6c691b3f4fee76175ff5196629b0
mdenoyel <mdenoyel@student.42.fr>
2018-03-11 17:54:27 +0000

    ajout defines ok et error

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdenoyel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mdenoyel <mdenoyel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 15:32:21 by mdenoyel          #+#    #+#             */
-/*   Updated: 2016/02/07 15:26:48 by mdenoyel         ###   ########.fr       */
+/*   Updated: 2018/03/11 17:48:46 by mdenoyel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 8
+# define GNL_OK 0
+# define GNL_ERROR 1
 
 int		get_next_line(int const fd, char **line);
 
